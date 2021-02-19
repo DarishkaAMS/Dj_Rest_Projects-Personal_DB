dcadeb4881d120304c82d99ff7f2660419fe0c77
DarishkaAMS <d.nikolaienko@yahoo.co.uk>
2021-02-19 19:14:53 +0200

    Add class Champagne

--- champagne/models.py
+++ champagne/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
-# Create your models here.
+Customer = get_user_model()
+
+
+class Champagne(models.Model):
+    brand = models.CharField(verbose_name='Brand', db_index=True, max_length=25)
+    color = models.CharField(verbose_name='Color', max_length=25)
+    size = models.CharField(verbose_name='Size', max_length=25)
+    CHAMP_TYPES = {
+        (1, 'Brut'),
+        (2, 'Extra Dry'),
+        (3, 'Dry'),
+        (4, 'Demi-Sec'),
+        (5, 'Doux'),
+    }
+    champ_type = models.IntegerField(verbose_name='Champagne Type', choices=CHAMP_TYPES)
+    customer = models.ForeignKey(Customer, verbose_name="Customer", on_delete=models.CASCADE)
