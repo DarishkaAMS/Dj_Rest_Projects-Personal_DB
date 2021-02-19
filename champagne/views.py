d019cb5d378d4508010faa6cdfb98ee9f79d0d6a
DarishkaAMS <d.nikolaienko@yahoo.co.uk>
2021-02-19 19:38:38 +0200

    Add ChampagneCreateView

--- champagne/views.py
+++ champagne/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from rest_framework import generics
+from champagne.serializer import ChampagneDetailSerializer
 
 
 class ChampagneCreateView(generics.CreateAPIView):
-    serializer_class = ...
+    serializer_class = ChampagneDetailSerializer
