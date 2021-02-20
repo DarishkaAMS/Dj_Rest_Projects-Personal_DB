a4d8a84aedbebe45240737b136a059ff12a2f849
DarishkaAMS <d.nikolaienko@yahoo.co.uk>
2021-02-20 18:17:59 +0200

    Add ChampagneDetailView

--- champagne/views.py
+++ champagne/views.py
@@ -4,10 +4,17 @@ from champagne.serializer import ChampagneDetailSerializer, ChampagneListSeriali
 from champagne.models import Champagne
 
 
+class ChampagneListView(generics.ListAPIView):
+    serializer_class = ChampagneListSerializer
+    queryset = Champagne.objects.all()
+
+
 class ChampagneCreateView(generics.CreateAPIView):
     serializer_class = ChampagneDetailSerializer
 
 
-class ChampagneListView(generics.ListAPIView):
-    serializer_class = ChampagneListSerializer
+class ChampagneDetailView(generics.RetrieveUpdateDestroyAPIView):
+    serializer_class = ChampagneDetailSerializer
     queryset = Champagne.objects.all()
+
+
