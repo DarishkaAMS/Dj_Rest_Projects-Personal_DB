54ebd39344574cc761eda42afd9a5efffe73350f
DarishkaAMS <d.nikolaienko@yahoo.co.uk>
2021-02-20 17:46:01 +0200

    Add champagne urls

--- champagne/urls.py
+++ champagne/urls.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
+from champagne.views import *
+
+app_name = 'champagne'
 
 urlpatterns = [
     # path('admin/', admin.site.urls),
-    # path('api/v1/champagne/', include('champagne.urls')),
+    path('champagne/create', ChampagneCreateView.as_view()),
 ]
