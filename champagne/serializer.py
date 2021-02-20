f49208176fb404adedf350b8fbf2f43ec3daab8f
DarishkaAMS <d.nikolaienko@yahoo.co.uk>
2021-02-20 18:12:49 +0200

    Add ChampagneListSerializer

--- champagne/serializer.py
+++ champagne/serializer.py
@@ -2,6 +2,12 @@ from rest_framework import serializers
 from champagne.models import Champagne
 
 
+class ChampagneListSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Champagne
+        fields = ('id', 'brand', 'champ_type')
+
+
 class ChampagneDetailSerializer(serializers.ModelSerializer):
     class Meta:
         model = Champagne
--- champagne/views.py
+++ champagne/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
 from rest_framework import generics
 from champagne.serializer import ChampagneDetailSerializer
+from champagne.models import Champagne
 
 
 class ChampagneCreateView(generics.CreateAPIView):
     serializer_class = ChampagneDetailSerializer
+
+class ChampagneListView(generics.ListAPIView):
+    serializer_class =
+    queryset = Champagne.objects.all()
